818a3eca373c844262f7207e7f6688fe31b1b1c6
zeyadkhaled1 <zeyadkhaled20016@gmail.com>
2021-12-19 22:49:24 +0200

    Create check_errors.h

---
+++ Headers/check_errors.h
@@ -0,0 +1,11 @@
+#ifndef CHECK_ERRORS_H
+#define CHECK_ERRORS_H
+#include<iostream>
+#include<string>
+#include<stack>
+#include<vector>
+
+using namespace std;
+
+string Check_XML_Errors(string xml_file);
+#endif // CHECK_ERRORS_H
